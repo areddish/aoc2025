a7b5130716fcf4bc2acc75e2ae6a5767086fb3b7
Andrew Reddish <areddish@foosion.net>
2025-12-11 11:18:39 -0500

    Day 11 solution

--- day11/day11.py
+++ day11/day11.py
@@ -1,35 +1,48 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from aoc_helper import *
-import heapq
 
-def bfs(nodes):
-    Q = []
-    for n in nodes["you"]:
-        Q.append((n, ["you"]))
+memo = {}
+def bfs_dp(nodes, current, hasFft, hasDac):
+    if (current, hasFft, hasDac) in memo:
+        return memo[(current, hasFft, hasDac)]
+    
+    if current == "out":
+        result = 1 if hasDac and hasFft else 0
+        memo[(current, hasFft, hasDac)] = result
+        return result
+    
+    hasFft |= current == "fft"
+    hasDac |= current == "dac"
+
+    result = 0
+    for dest in nodes[current]:
+        result += bfs_dp(nodes, dest, hasFft, hasDac)
+
+    memo[(current, hasFft, hasDac)] = result
+    return result
+
+def bfs(nodes, start="you", end="out"):
+    Q = deque(nodes[start])
 
     visited = set()
-    count = 0
-    count2 = 0
-    
+    count = 0  
     while Q:
-        print(Q)
-        dest, path = Q.pop(0)
+        dest = Q.popleft()
 
-        if dest == "out":
-            print(path)
+        if dest == end:
             count += 1
-            if "dac" in path and "fft" in path:
-                count2 += 1
+            continue
 
         visited.add(dest)
         for n in nodes[dest]:
             if n not in visited:
-                Q.append((n, [n] + list(path)))
-        
-    return count, count2
+                Q.append(n)
 
-with open("test.txt") as file:
-#with open("day11.txt") as file:
+    return count
+
+#with open("test.txt") as file:
+#with open("test2.txt") as file:
+with open("day11.txt") as file:
     lines = file.read().strip().splitlines()
 
     nodes = defaultdict(list)
@@ -38,6 +51,5 @@ with open("test.txt") as file:
         for dest in p[1:]:
             nodes[p[0][:-1]].append(dest)
 
-    p1, p2 = bfs(nodes)
-    answer(p1)
-    answer(p2)
+    answer(bfs(nodes))
+    answer(bfs_dp(nodes, "svr", False, False))
